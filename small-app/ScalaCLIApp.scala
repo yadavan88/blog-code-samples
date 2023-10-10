dd0246d630a15cfa10a290b3b83e723f74bb7b8f
Yadukrishnan <yadavan88@gmail.com>
2023-10-10 21:59:47 +0200

    Added scala-cli app sample

---
+++ small-app/ScalaCLIApp.scala
@@ -0,0 +1,15 @@
+//> using scala "3.3.1"
+//> using dep com.lihaoyi::os-lib:0.9.1
+package com.yadavan88.scalacli
+import os._
+// use the following command to create packaged app
+// scala-cli --power package ScalaCLIApp.scala -o smallApp
+object ScalaCliApp {
+  @main def app() = {
+    val path = os.pwd.toString
+    println(s"""
+               | Hello from the scala-cli packaged app!
+               | Current Path: ${path}
+                """.stripMargin)
+  }
+}
