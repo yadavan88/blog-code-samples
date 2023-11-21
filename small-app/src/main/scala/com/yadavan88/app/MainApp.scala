a4d29b9feb7bed8e6360bc07242e5a94184214dd
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-11-21 20:26:41 +0100

    Update scalafmt-core to 3.7.17 (#108)
    * Update scalafmt-core to 3.7.17
    
    * Reformat with scalafmt 3.7.17
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.7.17' to .git-blame-ignore-revs

--- small-app/src/main/scala/com/yadavan88/app/MainApp.scala
+++ small-app/src/main/scala/com/yadavan88/app/MainApp.scala
@@ -3,8 +3,7 @@ package com.yadavan88.app
 @main
 def mainMethod() = {
   val path = os.pwd.toString
-  println(
-    s"""
+  println(s"""
        | Hello from the packaged app!
        | Current Path: ${path}
       """.stripMargin)
